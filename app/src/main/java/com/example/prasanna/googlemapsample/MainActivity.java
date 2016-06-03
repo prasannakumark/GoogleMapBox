5268b68a6809f5fad7c4226faf6aa889decd53aa
Prasanna Kumar <prasanna.kumar@mobinius.com>
2016-06-03 19:02:41 +0530

    Bug fixes

--- app/src/main/java/com/example/prasanna/googlemapsample/MainActivity.java
+++ app/src/main/java/com/example/prasanna/googlemapsample/MainActivity.java
@@ -5,20 +5,37 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
-import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdate;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
-import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
+
+import com.mapbox.mapboxsdk.offline.OfflineManager;
+import com.mapbox.mapboxsdk.offline.OfflineRegion;
+import com.mapbox.mapboxsdk.offline.OfflineRegionError;
+import com.mapbox.mapboxsdk.offline.OfflineRegionStatus;
+import com.mapbox.mapboxsdk.offline.OfflineTilePyramidRegionDefinition;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    private boolean isEndNotified;
+    private ProgressBar progressBar;
     private MapView mapView;
+    // JSON encoding/decoding
+    public final static String JSON_CHARSET = "UTF-8";
+    public final static String JSON_FIELD_REGION_NAME = "FIELD_REGION_NAME";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onMapReady(final MapboxMap mapboxMap) {
                 mapboxMap.setMyLocationEnabled(true);
+
                 Log.v("TAG","Current Location"+mapboxMap.getMyLocation().getLatitude());
                 Log.v("TAG","Current Location"+mapboxMap.getMyLocation().getLongitude());
                 mapboxMap.setOnMapClickListener(new MapboxMap.OnMapClickListener() {
@@ -37,8 +55,8 @@ public class MainActivity extends AppCompatActivity {
                         Log.v("TAG","Current Location"+point.getLatitude());
 
                         LatLng latLng = new LatLng(point.getLatitude(), point.getLongitude());
-                        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 22);
-                        mapboxMap.animateCamera(cameraUpdate);
+                        /*CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, (float) (mapboxMap.getCameraPosition().zoom));
+                        mapboxMap.animateCamera(cameraUpdate);*/
                         mapboxMap.addMarker(new MarkerOptions()
                                 .position(point)
                                 .title("You are here"));
@@ -46,14 +64,91 @@ public class MainActivity extends AppCompatActivity {
                 });
                 Location location = mapboxMap.getMyLocation();
                 LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 22);
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 19);
                 mapboxMap.animateCamera(cameraUpdate);
             }
 
         });
-        //mapView.setStyleUrl(Style.MAPBOX_STREETS);
 
+        // Set up the OfflineManager
+        OfflineManager offlineManager = OfflineManager.getInstance(this);
+        offlineManager.setAccessToken(getResources().getString(R.string.accessToken));
 
+        // Create a bounding box for the offline region
+        LatLngBounds latLngBounds = new LatLngBounds.Builder()
+                .include(new LatLng(37.7897, -119.5073)) // Northeast
+                .include(new LatLng(37.6744, -119.6815)) // Southwest
+                .build();
+
+        // Define the offline region
+        OfflineTilePyramidRegionDefinition definition = new OfflineTilePyramidRegionDefinition(
+                mapView.getStyleUrl(),
+                latLngBounds,
+                10,
+                20,
+                this.getResources().getDisplayMetrics().density);
+
+        // Set the metadata
+        byte[] metadata;
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("Bangalore", "National Park");
+            String json = jsonObject.toString();
+            metadata = json.getBytes(JSON_CHARSET);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to encode metadata: " + e.getMessage());
+            metadata = null;
+        }
+
+        // Create the region asynchronously
+        /*offlineManager.createOfflineRegion(definition, metadata, new OfflineManager.CreateOfflineRegionCallback() {
+            @Override
+            public void onCreate(OfflineRegion offlineRegion) {
+                offlineRegion.setDownloadState(OfflineRegion.STATE_ACTIVE);
+
+                // Display the download progress bar
+                progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+                startProgress();
+
+                // Monitor the download progress using setObserver
+                offlineRegion.setObserver(new OfflineRegion.OfflineRegionObserver() {
+                    @Override
+                    public void onStatusChanged(OfflineRegionStatus status) {
+
+                        // Calculate the download percentage and update the progress bar
+                        double percentage = status.getRequiredResourceCount() >= 0 ?
+                                (100.0 * status.getCompletedResourceCount() / status.getRequiredResourceCount()) :
+                                0.0;
+
+                        if (status.isComplete()) {
+                            // Download complete
+                            endProgress("Region downloaded successfully.");
+                        } else if (status.isRequiredResourceCountPrecise()) {
+                            // Switch to determinate state
+                            setPercentage((int) Math.round(percentage));
+                        }
+                    }
+
+                    @Override
+                    public void onError(OfflineRegionError error) {
+                        // If an error occurs, print to logcat
+                        Log.e(TAG, "onError reason: " + error.getReason());
+                        Log.e(TAG, "onError message: " + error.getMessage());
+                    }
+
+                    @Override
+                    public void mapboxTileCountLimitExceeded(long limit) {
+                        // Notify if offline region exceeds maximum tile count
+                        Log.e(TAG, "Mapbox tile count limit exceeded: " + limit);
+                    }
+                });
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.e(TAG, "Error: " + error);
+            }
+        });*/
     }
 
     @Override
@@ -86,5 +181,30 @@ public class MainActivity extends AppCompatActivity {
         mapView.onSaveInstanceState(outState);
     }
 
+    // Progress bar methods
+    private void startProgress() {
 
+        // Start and show the progress bar
+        isEndNotified = false;
+        progressBar.setIndeterminate(true);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void setPercentage(final int percentage) {
+        progressBar.setIndeterminate(false);
+        progressBar.setProgress(percentage);
+    }
+
+    private void endProgress(final String message) {
+        // Don't notify more than once
+        if (isEndNotified) return;
+
+        // Stop and hide the progress bar
+        isEndNotified = true;
+        progressBar.setIndeterminate(false);
+        progressBar.setVisibility(View.GONE);
+
+        // Show a toast
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+    }
 }
